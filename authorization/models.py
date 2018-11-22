400bac0cfd27fd90ca0a9a2d6fc5ef1f2ad5114d
Denny.Chen <Denny.Chen@hopebaytech.com>
2018-11-22 22:48:14 +0800

    fix typo

--- authorization/models.py
+++ authorization/models.py
@@ -7,8 +7,12 @@ class authorizationModel(models.Model):
 	account = models.CharField(max_length=10, null=False)
 	password = models.CharField(max_length=50, null=False)
 	name = models.CharField(max_length=10, null=False)
-	line_ie = models.CharField(max_length=50, null=True)
+	line_id = models.CharField(max_length=50, null=True)
 	enable = models.BooleanField(default=False)
+
+	class Meta:
+		db_table = 'authorization'
 
 	def __str__(self):
 		return self.field_name
+		#return 'account=%s, password=%s, name=%s, line_id=%s' % (self.account, self.password, self.name, self.line_id)
