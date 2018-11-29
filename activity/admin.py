b0a6b7694aa8096d6b11fb6d173d48cde243bc97
Denny.Chen <Denny.Chen@hopebaytech.com>
2018-11-29 21:53:46 +0800

    combine_two_codes

--- activity/admin.py
+++ activity/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from activity.models import classSummaryModel, signUpModel
+
 # Register your models here.
-admin.site.register(classSummaryModel)
-admin.site.register(signUpModel)
--- activity/views.py
+++ activity/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from activity.models import classSummaryModel
+import random, datetime, requests
 
 # Create your views here.
+def info(request):
+    number = random.randint(1,100)
+    return render(request,"dice.html",{"info":number})
+    #return HttpResponse("Hello Django!!")
+
+def summary(request):
+    name = 'Jason'
+    number = random.randint(1,100)
+    search_time = datetime.datetime.now()
+    return render(request,"summary.html",{"name":name, "number":number, "search_time":search_time})
